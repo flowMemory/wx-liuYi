70db7174e4067930886a138338c7050dc8657f82
flowMemory <772675479@qq.com>
2020-05-08 16:22:50 +0800

    wx-liuyi study

--- http/http-pro.js
+++ http/http-pro.js
@@ -1,26 +1,27 @@
 // 封装 http ajax 请求基类
 import config from '../config/config.js'
 class HTTP {
-  // constructor()
-  request(params) {
+    request({url, data={}, method='GET'}) {
 		return Promise((resolve, reject) => {
-			this._request()
+			this._request(url, data, resolve, reject, data, method)
 		})
-		_request() {
-			wx.request({
-				url: congfig.base_url + params.url,
-				header: {
-					key: config.key
-				},
-				data: params.data,
-				success: (res => {
-					params.success(res)
-				}),
-				fail: (error) => {
-					console.log(error)
-				}
-			})
-		}
-  }
+	}
+	_request(url, data, resolve, reject, data={}, method='GET') {
+		wx.request({
+			url: congfig.base_url + url,
+			method: method,
+			header: {
+				key: config.key
+			},
+			data: data,
+			success: (res => {
+				resolve(res)
+			}),
+			fail: (error) => {
+				reject()
+				console.log(error)
+			}
+		})
+	}
 }
 export default HTTP;
